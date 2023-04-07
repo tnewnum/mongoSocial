cc05813bbc213c0f8da8b30922ee27b6293eca73
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-07 13:54:44 -0400

    refactor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
+
 
 const PORT = process.env.port || 3001;
 const app = express();
--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,26 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp)
+  }
+});
+
 const thoughtSchema = new Schema (
     {
         thoughtText: {
@@ -32,26 +53,6 @@ thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
-const reactionSchema = new Schema({
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (timestamp) => dateFormat(timestamp)
-    }
-  });
 
 const Thought = model('thought', thoughtSchema);
 
